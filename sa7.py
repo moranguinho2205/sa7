54f8d706c7282a304be8c9da107d47dbdc16155e
moranguinho2205 <120068922+moranguinho2205@users.noreply.github.com>
2022-12-07 22:08:10 -0300

    Update sa7.py

---
+++ sa7.py
@@ -0,0 +1,9 @@
+aviao = []
+print ("escolha uma opcao:")
+print("opcao[1] - Registrar o numero do aviao: ")
+print("opcao[2] - Registrar a quantidade de assentos disponiveis:")
+print("opcao[3] - Reservar passagem aérea:")
+print("opcao[4] - Realizar consulta de avião:")
+print("opcao[5] - Realizar consulta de passageiro:")
+print("opcao[6] - Finalizar:")
+opcao_escolhida = int (input("opcao:"))
